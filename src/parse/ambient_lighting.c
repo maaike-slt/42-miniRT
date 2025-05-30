32bcdbd6a3e14527ebf8f0e212575f6f0fbbfb16
maaike-slt <maaike.sloot.26@gmail.com>
2025-05-30 22:10:52 +0200

    fix lil buggies

--- src/parse/ambient_lighting.c
+++ src/parse/ambient_lighting.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 21:45:35 by msloot            #+#    #+#             */
-/*   Updated: 2025/05/30 21:55:39 by msloot           ###   ########.fr       */
+/*   Updated: 2025/05/30 22:10:25 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,17 @@ A 0.2 255,255,255
 
 bool	parse_ambient_lighting(t_env *env, const t_line_arg *la)
 {
-	t_ambient	*al;
+	t_ambient	*a;
 
 	if (la->split_size != 3)
 		return (puterr_invalid_token_amount(\
 la, 3, "<lighting ratio> <color in r,g,b>"), false);
-	al = &(env->scene.a);
-	al->lighting_ratio = ft_atof(la->split[1]);
-	if (al->lighting_ratio < 0 || al->lighting_ratio > 1)
+	a = &(env->scene.a);
+	a->lighting_ratio = ft_atof(la->split[1]);
+	if (a->lighting_ratio < 0 || a->lighting_ratio > 1)
 		return (\
-puterr_invalid_float_positive_non_null(la, la->split[2]), false);
-	if (!parse_color(la, la->split[2], &(al->color)))
+puterr_invalid_float(la, la->split[2], 0.0, 1.0), false);
+	if (!parse_color(la, la->split[2], &(a->color)))
 		return (false);
 	env->scene.sp_amt++;
 	return (true);
