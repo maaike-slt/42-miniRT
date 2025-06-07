a2f03d39b70158038bbb860e8339deddbbddd4a7
adelille <adelille@student.42.fr>
2025-06-07 23:44:47 +0200

    remove trailing white space

--- src/render/color_ratio.c
+++ src/render/color_ratio.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 11:07:13 by adelille          #+#    #+#             */
-/*   Updated: 2025/05/31 11:10:14 by adelille         ###   ########.fr       */
+/*   Updated: 2025/06/07 23:44:32 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /* (color ratio)
  Converts a color value from t_color_bit (0-255) to a float (0.0-1.0).
- 
+
  @param c The color value in t_color_bit format.
  @return The color value as a float.
 */
@@ -25,7 +25,7 @@ float	cr(t_color_bit c)
 
 /* (color ratio reverse)
  Converts a float color value (0.0-1.0) to t_color_bit (0-255).
- 
+
  @param c The color value as a float.
  @return The color value in t_color_bit format.
 */
